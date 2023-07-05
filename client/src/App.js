5bf1512f088feda25286e9196e3b22d9660c7a87
aayushgunner <luitelaayush8@gmail.com>
2023-07-05 21:01:22 +0545

    Table in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import DataTable from './DataTable';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API
+    axios.get('http://localhost:8000/records')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <DataTable />
+    <div className="container">
+      <h1>JSON Data Table</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.first_name}</td>
+              <td>{item.last_name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- main.go
+++ main.go
@@ -1,72 +1,138 @@
-package main
+// package main
  
+// import (
+//     "database/sql"
+//     // "fmt"
+// 	"encoding/json"
+//     _ "github.com/lib/pq"
+// 	"net/http"
+// 	"log"
+// )
+
+// type players struct {
+// 	FirstName string `json:"first_name"`
+// 	LastName  string `json:"last_name"`
+// }
+ 
+// const (
+//     host     = "localhost"
+//     port     =  5432
+//     user     = "postgres"
+//     password = "golyanglyang"
+//     dbname   = "try"
+// )
+
+// func main() {
+// http.HandleFunc("/records", retrieveRecord)
+// err := http.ListenAndServe(":8000", nil)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+	
+// }
+
+// func retrieveRecord(w http.ResponseWriter, r *http.Request) {
+// 	// Database connection string
+// 	psqlconn := "host=localhost port=5432 user=postgres password=golyanglyang dbname=try sslmode=disable"
+	
+// 	// Open the database connection
+// 	db, err := sql.Open("postgres", psqlconn)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// 	defer db.Close()
+
+// 	// Execute the SELECT query
+// 	rows, err := db.Query("SELECT first_name, last_name FROM players")
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// 	defer rows.Close()
+
+// 	// Create a slice to store the retrieved records
+// 	students := []players{}
+
+// 	// Iterate over the rows and populate the slice
+// 	for rows.Next() {
+// 		var student players
+// 		err := rows.Scan( &student.FirstName, &student.LastName)
+// 		if err != nil {
+// 			log.Println(err)
+// 			continue
+// 		}
+// 		students = append(students, student)
+// 	}
+
+// 	// Check for any errors encountered during row iteration
+// 	if err := rows.Err(); err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	// Convert the data to JSON
+// 	jsonData, err := json.Marshal(students)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	// Set the Content-Type header to application/json
+// 	w.Header().Set("Content-Type", "application/json")
+
+// 	// Write the JSON response
+// 	w.Write(jsonData)
+	
+// 	}
+ 
+// func CheckError(err error) {
+//     if err != nil {
+//         panic(err)
+//     }
+// }
+
+package main
+
 import (
-    "database/sql"
-    // "fmt"
+	"database/sql"
 	"encoding/json"
-    _ "github.com/lib/pq"
-	"net/http"
+	"fmt"
 	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
-type players struct {
+type Player struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 }
- 
+
 const (
-    host     = "localhost"
-    port     =  5432
-    user     = "postgres"
-    password = "golyanglyang"
-    dbname   = "try"
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "golyanglyang"
+	dbname   = "try"
 )
 
 func main() {
-// //         // connection string
-// //     psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-         
-// //         // open database
-// //     db, err := sql.Open("postgres", psqlconn)
-// //     CheckError(err)
-     
-// //         // close database
-// //     defer db.Close()x
- 
-// //         // check db
-// //     err = db.Ping()
-// //     CheckError(err)
- 
-// //     fmt.Println("Connected!")
-// // 	rows, err := db.Query(`SELECT "first_name", "last_name" FROM "players"`)
-// //     CheckError(err)
- 
-// //    defer rows.Close()
-// //    for rows.Next() {
-// //     var first_name string
-// //     var last_name string
- 
-// //     err = rows.Scan(&first_name, &last_name)
-// //     CheckError(err)
- 
-// //     fmt.Println(first_name, last_name)
-
-	 
-// }
- 
-// CheckError(err)
-http.HandleFunc("/records", retrieveRecord)
-err := http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/records", retrieveRecord)
+	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
 }
 
 func retrieveRecord(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	// Database connection string
-	psqlconn := "host=localhost port=5432 user=postgres password=golyanglyang dbname=try sslmode=disable"
-	
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
 	// Open the database connection
 	db, err := sql.Open("postgres", psqlconn)
 	if err != nil {
@@ -84,17 +150,17 @@ func retrieveRecord(w http.ResponseWriter, r *http.Request) {
 	defer rows.Close()
 
 	// Create a slice to store the retrieved records
-	students := []players{}
+	players := []Player{}
 
 	// Iterate over the rows and populate the slice
 	for rows.Next() {
-		var student players
-		err := rows.Scan( &student.FirstName, &student.LastName)
+		var player Player
+		err := rows.Scan(&player.FirstName, &player.LastName)
 		if err != nil {
 			log.Println(err)
 			continue
 		}
-		students = append(students, student)
+		players = append(players, player)
 	}
 
 	// Check for any errors encountered during row iteration
@@ -104,7 +170,7 @@ func retrieveRecord(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Convert the data to JSON
-	jsonData, err := json.Marshal(students)
+	jsonData, err := json.Marshal(players)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -115,11 +181,4 @@ func retrieveRecord(w http.ResponseWriter, r *http.Request) {
 
 	// Write the JSON response
 	w.Write(jsonData)
-	
-	}
- 
-func CheckError(err error) {
-    if err != nil {
-        panic(err)
-    }
 }
