3543867dc2b4eef30c7a5852b06ecd7dc489b5f9
PasangSherpa123 <pasangsherpa12580@gmail.com>
2023-07-11 15:37:17 +0545

    data added

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"encoding/csv"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/handlers"
 	"football_server/pkg/routes"
 	"database/sql"
     _ "github.com/lib/pq"
 	"fmt"
+	"os"
+	"io"
+	"path/filepath"
 	"football_server/pkg/controllers"
 )
 var db *sql.DB
@@ -37,7 +41,43 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
+	absPath, err := filepath.Abs("cmd/clubInfo.csv")
+if err != nil {
+	log.Fatal(err)
+}
+file, err := os.Open(absPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Create a new CSV reader
+	reader := csv.NewReader(file)
 	controllers.SetDB(db)
+	for {
+		// Read each row from the CSV file
+		row, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+	
+		fmt.Println("helloworld")
+		// Prepare the SQL INSERT statement
+		stmt, err := db.Prepare("INSERT INTO team (team_name, abbreviation, hex_code, team_logo, Stadium) VALUES ($1, $2, $3, $4, $5)")
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		// Execute the INSERT statement with row data
+		fmt.Println(row[0], row[1], row[2], row[3], row[4])
+		_, err = stmt.Exec(row[0], row[1], row[2], row[3], row[4])
+		
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	router := mux.NewRouter()
 	routes.RegisterRoutes(router)
