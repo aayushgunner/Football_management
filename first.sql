3c7813c067a390007a4396776126b7c4f067422a
PasangSherpa123 <pasangsherpa12580@gmail.com>
2023-07-11 15:37:17 +0545

    data added

--- first.sql
+++ first.sql
@@ -8,7 +8,7 @@ CREATE TYPE player_position AS ENUM (
     'GoalKeeper',
     'Left Back',
     'Right Back',
-    'Centre Back'
+    'Centre Back',
     'Left Midfield',
     'Right Midfield',
     'Centre Midfield',
@@ -32,9 +32,10 @@ CREATE TABLE coach (
 CREATE TABLE team (
   team_id SERIAL PRIMARY KEY,
   team_name VARCHAR(50),
-  city VARCHAR(50),
-  coach_id INT,
-  FOREIGN KEY (coach_id) REFERENCES coach(coach_id)
+  team_logo VARCHAR(300),
+  hex_code VARCHAR(10),
+  abbreviation VARCHAR(9),
+  Stadium VARCHAR(50)
 );
 
 CREATE TABLE players (
@@ -44,37 +45,4 @@ CREATE TABLE players (
    position player_position,
    team_id INT,
    FOREIGN KEY (team_id) REFERENCES team(team_id)
-
 );
-
-
-
-
-
-
-INSERT into coach (first_name, last_name , age ) VALUES ('Mikel', 'Arteta',41);
-INSERT into coach (first_name, last_name , age ) VALUES ('Pep', 'Guardiola',51);
-INSERT into coach (first_name, last_name , age ) VALUES ('Jurgen', 'Klopp',54);
-
-
-
-INSERT INTO team (team_name , city , coach_id) VALUES('Arsenal', 'London', 1 );
-INSERT INTO team (team_name , city , coach_id) VALUES('Manchester City', 'Manchester', 2 );
-INSERT INTO team (team_name , city , coach_id) VALUES('Liverpool', 'Liverpool', 3 );
-
-
-INSERT into players(first_name,last_name,position,team_id) VALUES ('Bukayo', 'Saka'   ,'Right Wing', 1 );
-INSERT into players(first_name,last_name,position,team_id) VALUES ('Jack', 'Grealish', 'Left Wing', 2);
-INSERT into players(first_name,last_name,position,team_id) VALUES ('Darwin', 'Nunez' , 'Striker',3 );
-INSERT into players(first_name,last_name,position,team_id) VALUES ('Gabriel', 'Martinelli','Left Wing',1 );
-INSERT into players(first_name,last_name,position,team_id) VALUES ('Gabriel', 'Jesus','Striker', 1 );
-INSERT into players(first_name,last_name,position,team_id) VALUES ('Granit', 'Xhaka', 'Centre Midfield',1 );
-INSERT into players(first_name,last_name,position,team_id) VALUES ('Ben', 'White' , 'Right Back',1 );
-
-SELECT first_name , last_name FROM players;
-
-
-SELECT first_name , last_name FROM players where team_id IN (SELECT team_id FROM team WHERE coach_id IN (SELECT coach_id FROM coach WHERE first_name ='Mikel')) ORDER BY first_name ASC;
-SELECT first_name , last_name FROM players where team_id IN (SELECT team_id FROM team  WHERE team_name='Arsenal' ) ORDER BY first_name ASC;
-
-SELECT * FROM team
