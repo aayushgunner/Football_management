d229f53903b72b6c0afbe0120948e75df9ec0267
PasangSherpa123 <pasangsherpa12580@gmail.com>
2023-07-22 22:23:25 +0545

    Made changes to modal

--- client/src/screen/Stats.js
+++ client/src/screen/Stats.js
@@ -1,10 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../assets/styles/stats.css';
+// import '../assets/styles/stats.css';
 import Modal from 'react-modal';
 import footballImage from '../assets/images/saka.png';
 import { getPlayer, getStat, getTeam } from '../apiRoutes';
-
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width:'70%',
+    background:'grey',
+    height:'80%',
+    zIndex: '1000'
+  },
+};
 const Stats = () => {
   const [data, setData] = useState([]);
   const [showTable, setShowTable] = useState(false);
@@ -12,7 +25,20 @@ const Stats = () => {
   const [teams, setTeams] = useState([]);
   const [players, setPlayers] = useState([]);
   const [stats, setStats] = useState([]);
-  const [selectedPlayer, setSelectedPlayer] = useState(null);
+  // const [selectedPlayer, setSelectedPlayer] = useState(null);
+  // const []
+  const [selectedPlayer, setSelectedPlayer] = useState({
+    player_name: 'Bukayo Saka',
+    games: '12',
+    mins_played: '1234',
+    goal:'12',
+    assists:'12',
+    shots:'12',
+    key_passes: '1234',
+    yellow_cards: '123',
+    red_cards: '123',
+    position:'Forward'
+  })
 
   useEffect(() => {
     clearData();
@@ -139,66 +165,85 @@ const Stats = () => {
             ))}
           </table>
         </div>
-      )}
-
-      {selectedPlayer && (
-        <Modal
-        isOpen={selectedPlayer !== null}
+      )}  <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={true}
+        // onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
-        contentLabel="Player Image"
-        className="modal-container"
-        overlayClassName="modal-overlay"
+        style={customStyles}
+        contentLabel="Example Modal"
+        // className="Model"
       >
-        <div className="modal-content">
-          <div className="modal-image-container">
-            <img src={footballImage} alt="Football" className="modal-image" />
+       <div className="Model_Content">
+          <div className="Model_Content_ImageBox">
+            <img className="Model_Content_ImageBox_Image" src={footballImage} alt="Football" className="modal-image" />
           </div>
-          <div className="card-container">
+          <div className="Model_Content_CardContainer">
+            <div className="Model_Content_CardContainer_Cards">
             <div className="card">
               <h3>Player Name</h3>
-              <p>{selectedPlayer.player_name}</p>
+              <p>{selectedPlayer?.player_name}</p>
             </div>
             <div className="card">
               <h3>Games</h3>
-              <p>{selectedPlayer.games}</p>
+              <p>{selectedPlayer?.games}</p>
             </div>
             <div className="card">
               <h3>Minutes Played</h3>
-              <p>{selectedPlayer.mins_played}</p>
+              <p>{selectedPlayer?.mins_played}</p>
             </div>
             <div className="card">
               <h3>Goals</h3>
-              <p>{selectedPlayer.goal}</p>
+              <p>{selectedPlayer?.goal}</p>
             </div>
             <div className="card">
               <h3>Assists</h3>
-              <p>{selectedPlayer.assists}</p>
+              <p>{selectedPlayer?.assists}</p>
             </div>
+            </div>
+            <div className="Model_Content_CardContainer_Cards">
+           
             <div className="card">
               <h3>Shots</h3>
-              <p>{selectedPlayer.shots}</p>
+              <p>{selectedPlayer?.shots}</p>
             </div>
             <div className="card">
               <h3>Key Passes</h3>
-              <p>{selectedPlayer.key_passes}</p>
+              <p>{selectedPlayer?.key_passes}</p>
             </div>
             <div className="card">
               <h3>Yellow Cards</h3>
-              <p>{selectedPlayer.yellow_cards}</p>
+              <p>{selectedPlayer?.yellow_cards}</p>
             </div>
             <div className="card">
               <h3>Red Cards</h3>
-              <p>{selectedPlayer.red_cards}</p>
+              <p>{selectedPlayer?.red_cards}</p>
             </div>
             <div className="card">
               <h3>Position</h3>
-              <p>{selectedPlayer.position}</p>
+              <p>{selectedPlayer?.position}</p>
+            </div>
             </div>
           </div>
         </div>
       </Modal>
+    </div>
+
+
+      {/* {selectedPlayer && ( */}
+        <Modal
+        isOpen={true}
+        onRequestClose={closeModal}
+        contentLabel="Player Image"
+        // style={customStyles}
+        className="modal-container"
+        overlayClassName="modal-overlay"
+      >
+        
+      </Modal>
       
-      )}
+      {/* )} */}
     </div>
   );
 };
