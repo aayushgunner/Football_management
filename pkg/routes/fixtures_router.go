8276cb825a146faac96a39acf4530409368f5700
aayushgunner <luitelaayush8@gmail.com>
2023-08-23 22:48:37 +0545

    Fixtures

---
+++ pkg/routes/fixtures_router.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"football_server/pkg/controllers"
+)
+
+func FixturesRoutes(router *mux.Router) {
+		router.HandleFunc("/fixtures", controllers.RetrieveFixtures).Methods("GET")
+}
