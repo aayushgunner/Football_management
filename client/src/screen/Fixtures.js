56c6549305d18a7abd1829cea281b4387cd046d9
PasangSherpa123 <pasangsherpa12580@gmail.com>
2023-08-23 22:50:18 +0545

    make some changes

---
+++ client/src/screen/Fixtures.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Fixtures = () => {
+  return (
+    <div className='Fixtures'>
+        <div className='Fixtures_Box'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Fixtures
